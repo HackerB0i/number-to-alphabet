d3d6329408ec3b843c1d83aab2e53c47d5e0caf2
skonk <anntvfamily@gmail.com>
2024-03-25 19:50:04 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+thing for decoding matrices. top 10 most useless script
+"""
+
+import string
+
+askfornumber = True
+numberlist = []
+characters = ["0", 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+while askfornumber:
+    number = input("number: (s to stop) | ")
+    if (number == "s"):
+        askfornumber = False
+    else:
+        numberlist.append(number)
+
+print("your generated text is below:")
+text = ""
+for i in range(len(numberlist)):
+    text = text + characters[int(numberlist[i])]
+print(text)
